ef3b5c0d93ff87d57d32deaacf814285f5b69843
RadianceG <1054120576@qq.com>
2021-03-19 17:09:14 +0800

    just can run

---
+++ src/main/java/com/nic/test/Test.java
@@ -0,0 +1,13 @@
+package com.nic.test;
+
+import com.nic.model.*;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+public class Test {
+
+    public static void main(String[] args) throws ParserConfigurationException {
+
+    }
+}
+
